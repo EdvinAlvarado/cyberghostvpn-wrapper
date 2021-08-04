d645bebe2a759439e4baeff6c0076606487fbe2b
EdvinAlvarado <ealvarado301@gmail.com>
2021-08-03 19:24:00 -0700

    reworked cpp

--- vpn.cpp
+++ vpn.cpp
@@ -1,42 +1,49 @@
+#include <cstddef>
 #include <iostream>
+#include <iterator>
+#include <ostream>
 #include <string>
 #include <vector>
+#include <map>
 
 enum return_status {OK, PROGRAM_UNINSTALLED, WRONG_INPUT, WRONG_COMMAND, WRONG_CITY};
 
 int main(int argc, char* argv[]) {
-	std::vector<std::string> args(argv, argv+argc);
 	if (system("pacman -Qq cyberghostvpn")) {std::cerr << "cyberghostvpn is not installed" << std::endl; return PROGRAM_UNINSTALLED;}
-	switch (argc) {
-		case 2:
-			if 		(args[1] == "stop") {system("cyberghostvpn --stop"); return OK;}
-			else if (args[1] == "help") {system("cyberghostvpn --help"); return OK;}
-			else if (args[1] == "status") {system("cyberghostvpn --status"); return OK;}
-			break;	
-		case 3:
-			if (args[1] == "connect") {
-				std::string strC = "cyberghostvpn --traffic --connect --country-code ";
-				if (args[2].length() == 2) {
-					strC.append(args[2]);
-					system(strC.c_str());
-					return OK;
+
+	std::vector<std::string> args(argv, argv+argc);
+	std::string cmd = "";
+	const std::map<std::string, std::string> one = {{"stop", " --stop"}, {"help", " --help"}, {"status", " --status"}, {"connect", " --traffic --connect"}};
+	for (size_t i = 0; i < args.size(); i++) {
+		switch (i) {
+			case 0: {
+				cmd.append("cyberghostvpn");
+				break;
+			}
+			case 1: {
+				auto one_it = one.find(args[1]);
+				if (one_it != one.end()) {cmd.append(one_it->second);} else {return WRONG_INPUT;}
+				break;
+			}
+			case 2: {
+				if (args[1] == "connect") {
+					if (args[2].length() == 2) {cmd.append(" ").append(args[2]);}
+					else {std::cerr << "Wrong country code" << std::endl; return WRONG_INPUT;}
 				}
-				else {std::cerr << "Wrong country code" << std::endl; return WRONG_INPUT;}
+				else {std::cerr << "unsupported extra input" << std::endl; return WRONG_INPUT;}
+				break;
 			}
-			break;
-		case 4:
-			if (args[1] == "connect") {
-				std::string strC = "cyberghostvpn --traffic --connect --country-code ";
-				if (args[2].length() == 2) {strC.append(args[2]);}
-				else {std::cerr << "Wrong country code" << std::endl; return WRONG_INPUT;}
-				
-				strC.append(" --city '" + args[3] + "'");
-				if (system(strC.c_str())) {std::cerr << "Wrong city" << std::endl; return WRONG_CITY;}
-				else {return OK;}
-				// return (system(strC.c_str())) ? OK : WRONG_CITY;
+			case 3: {
+				if (args[1] == "connect") {cmd.append(" ").append(args[3]);}
+				else {std::cerr << "unsupported input" << std::endl; return WRONG_INPUT;}
+				break;
 			}
-			break;
+			default:
+				std::cerr << "too many inputs" << std::endl;
+				return WRONG_INPUT;
+		}
 	}
-	std::cerr << "wrong command" << std::endl;
-	return WRONG_COMMAND;
+	if (system(cmd.c_str())) {std::cerr << "wrong command" << std::endl; return WRONG_COMMAND;}
+	else {return OK;}
 }
+
