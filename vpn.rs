d97e3606db0482487dfba388a720d2ee8863e512
EdvinAlvarado <ealvarado301@gmail.com>
2021-10-14 23:12:21 -0500

    implemented readable errors & eliminated unneeded vector

--- vpn.rs
+++ vpn.rs
@@ -2,14 +2,25 @@ use std::process::Command;
 
 enum Status {ProgramNotInstalled=1, CommandFailed=2, CountryFailed=3, InputFailed=4}
 
-fn main() -> Result<(), i32> {
+impl Status {
+    fn as_str(self) -> &'static str {
+        match self {
+            Self::ProgramNotInstalled   => "Program Not Installed",
+            Self::CommandFailed         => "Command Failed",
+            Self::CountryFailed         => "Counry Failed",
+            Self::InputFailed           => "Input Failed",
+        }
+    }
+}
+
+fn main() -> Result<(), &'static str> {
     // Command | modifier | modifier | Result<Child> | Child | Result<ExitStatus> | ExitStatus | bool
-    if !Command::new("pacman").arg("-Qq").arg("cyberghostvpn").spawn().expect("cyberghostvpn is not installed").wait().expect("check wait failed").success() {return Err(Status::ProgramNotInstalled as i32);}
+    if !Command::new("pacman").arg("-Qq").arg("cyberghostvpn").spawn().expect("cyberghostvpn is not installed").wait().expect("check wait failed").success() {return Err(Status::ProgramNotInstalled.as_str());}
     
     let mut vpn = Command::new("cyberghostvpn");
-    let arg_vec: Vec<String> = std::env::args().collect();
+    // let arg_vec: Vec<String> = std::env::args().collect();
 
-    for (i, ar) in arg_vec.iter().enumerate() {
+    for (i, ar) in std::env::args().enumerate() {
         match i {
             0 => {},
             1 => {
@@ -18,13 +29,13 @@ fn main() -> Result<(), i32> {
                     "help"      => {vpn.arg("--stop");},
                     "status"    => {vpn.arg("--status");},
                     "connect"   => {vpn.arg("--traffic").arg("--connect").arg("--country-code");},
-                    _           => return Err(Status::InputFailed as i32),
+                    _           => return Err(Status::InputFailed.as_str()),
                 }
             },
-            2 => {if ar.len() == 2 {vpn.arg(&ar);} else {return Err(Status::CountryFailed as i32);}},
+            2 => {if ar.len() == 2 {vpn.arg(&ar);} else {return Err(Status::CountryFailed.as_str());}},
             3 => {vpn.arg("--city").arg(format!("'{}'", ar));},
-            _ => {return Err(Status::InputFailed  as i32);}
+            _ => {return Err(Status::InputFailed.as_str());}
         }
     }
-    if vpn.spawn().expect("process failed").wait().expect("Wait error").success() {return Ok(());} else {return Err(Status::CommandFailed as i32);}
+    if vpn.spawn().expect("process failed").wait().expect("Wait error").success() {return Ok(());} else {return Err(Status::CommandFailed.as_str());}
 }
